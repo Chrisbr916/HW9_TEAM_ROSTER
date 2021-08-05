402ff30e6bb9f2d22810eb7b49d220c8b61489fd
Puma Prince <christopherbreen22@gmail.com>
2021-08-04 21:08:00 -0400

    added all the arrays to fill in the information

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
+let teamArray = [];
 
 
 
-const generateHTML = (answers) => 
+const generateHTML = () => 
 
    ` <!DOCTYPE html>
     <html lang="en">
@@ -24,16 +25,9 @@ const generateHTML = (answers) =>
             </div>
           </nav>
     
-          <div class="card" style="width: 18rem; padding: 10px; margin: 120px;">
-        <div class="card-header bg-primary text-white">
-          ${answers.position}
-        </div>
-        <ul class="list-group list-group-flush">
-          <li class="list-group-item">${answers.ID}</li>
-          <li class="list-group-item">${answers.email}</li>
-          <li class="list-group-item">https://github.com/${answers.git}</li>
-        </ul>
-      </div>
+         ${teamArray.map(card => {
+             return card;
+         })}
          
     
     
@@ -48,7 +42,7 @@ const generateHTML = (answers) =>
     </html>`;
  
 
-
+function promptEmployeeType(){
 inquirer
     .prompt([
 
@@ -60,31 +54,189 @@ inquirer
 
         },
 
-        {
-            type: "input",
-            name: "ID",
-            message: "Please enter their employee ID?",
-            validate: (value) =>{if(value){return true} else{return 'please enter a value to continue'}} 
-        },
-        {
-            type: "input",
-            name: "email",
-            message: "Please enter their email",
-            validate: (value) => {if(value){return true} else{return "please enter a value to continue."}}
-        },
-        {
-            type: "input", 
-            name: "git",
-            message: "Please enter their github username",
-            validate: (value) => {if(value){return true} else{return "please enter a value to continue."}}
-        },
+        // {
+        //     type: "input",
+        //     name: "ID",
+        //     message: "Please enter their employee ID?",
+        //     validate: (value) =>{if(value){return true} else{return 'please enter a value to continue'}} 
+        // },
+        // {
+        //     type: "input",
+        //     name: "email",
+        //     message: "Please enter their email",
+        //     validate: (value) => {if(value){return true} else{return "please enter a value to continue."}}
+        // },
+        // {
+        //     type: "input", 
+        //     name: "git",
+        //     message: "Please enter their github username",
+        //     validate: (value) => {if(value){return true} else{return "please enter a value to continue."}}
+        // },
     ])
 
 
     .then((answers) => {
-        const HTMLContent = generateHTML(answers);
+        if(answers.position==="Manager") {
+            addManager()
+            
+        }
+        else if(answers.position==="Engineer"){
+            addEngineer()
+        }
+        else if(answers.position==="Intern"){
+            addIntern()
+        }
+        else {saveTeam()}
+       
+    });
+}
+
+    function saveTeam() {
+        const HTMLContent = generateHTML();
 
         fs.writeFile("index.html" , HTMLContent, (err) => 
         err ? console.log(err) : console.log('congrats you have created a employee roster!!')
         );
-    });
+    }
+
+    function addManager(){
+        inquirer.prompt([
+
+            {
+                type: "input",
+                message: "what is their name",
+                name:"name",
+                validate: (value) =>{if(value){return true} else{return 'please enter a value to continue'}}
+            },
+            {
+                type: "input",
+                message: "Please enter the employee ID",
+                name:"ID",
+                validate: (value) =>{if(value){return true} else{return 'please enter a value to continue'}}
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "Please enter their email",
+                validate: (value) =>{if(value){return true} else{return 'please enter a value to continue'}}
+            },
+            {
+            type: "input",
+            name: "office",
+            message: "What is their office number",
+            validate: (value) =>{if(value){return true} else{return 'please enter a value to continue'}}
+            }
+
+        ])
+        .then(answers => {
+           // console.log(answers);
+            let card = ` <div class="card" style="width: 18rem; padding: 10px; margin: 40px;">
+            <div class="card-header bg-primary text-white">
+            ${answers.name}\   
+           
+            Manager
+            </div>
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item">ID: ${answers.ID}</li>
+              <li class="list-group-item">Email: ${answers.email}</li>
+              <li class="list-group-item">Office number: ${answers.office}</li>
+            </ul>
+          </div>`
+          teamArray.push(card)
+            promptEmployeeType()
+        })
+    }
+   function addEngineer(){
+       inquirer.prompt([
+
+        {
+            type: "input",
+            name: "name",
+            message: "Please enter the name of the engineer",
+            validate: (value) =>{if(value){return true} else{return 'please enter a value to continue'}}
+        },
+        {
+            type: "input",
+            name: "ID",
+            message: "Please enter employee ID",
+            validate: (value) =>{if(value){return true} else{return 'please enter a value to continue'}}
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Please enter email",
+            validate: (value) =>{if(value){return true} else{return 'please enter a value to continue'}}
+        },
+        {
+            type: "input",
+            name: "git",
+            message: "Please enter employees github username",
+            validate: (value) =>{if(value){return true} else{return 'please enter a value to continue'}}
+        }
+    
+       ])
+       .then(answers => {
+           let card = `<div class="card" style="width: 18rem; padding: 10px; margin: 40px;">
+           <div class="card-header bg-primary text-white">
+           ${answers.name}\   
+          
+           Engineer
+           </div>
+           <ul class="list-group list-group-flush">
+             <li class="list-group-item">ID: ${answers.ID}</li>
+             <li class="list-group-item">Email: ${answers.email}</li>
+             <li class="list-group-item">https://github.com/${answers.git}</li>
+           </ul>
+         </div>`
+         teamArray.push(card)
+            promptEmployeeType()
+       })
+
+    }
+    function addIntern(){
+        inquirer.prompt([
+
+            {
+                type: "input",
+                name: "name",
+                message: "Please enter the name of the employee",
+                validate: (value) =>{if(value){return true} else{return 'please enter a value to continue'}}
+            },
+            {
+                type: "input",
+                name: "ID",
+                message: "Please enter employee ID",
+                validate: (value) =>{if(value){return true} else{return 'please enter a value to continue'}}
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "Please enter the email",
+                validate: (value) =>{if(value){return true} else{return 'please enter a value to continue'}}
+            },
+            {
+                type: "input",
+                name: "school",
+                message: "please enter the school that the student is attending",
+                validate: (value) =>{if(value){return true} else{return 'please enter a value to continue'}}
+            }
+        ])
+        .then(answers => {
+            let card = `<div class="card" style="width: 18rem; padding: 10px; margin: 40px;">
+           <div class="card-header bg-primary text-white">
+           ${answers.name}\   
+          
+           Intern
+           </div>
+           <ul class="list-group list-group-flush">
+             <li class="list-group-item">ID: ${answers.ID}</li>
+             <li class="list-group-item">Email: ${answers.email}</li>
+             <li class="list-group-item">School: ${answers.school}</li>
+           </ul>
+         </div>`
+         teamArray.push(card)
+            promptEmployeeType()
+        })
+    }
+
+    promptEmployeeType()
---
+++ main.test.js
@@ -0,0 +1,6 @@
+const main = require ('../main');
+
+describe('main', () => { 
+
+    it()
+})
